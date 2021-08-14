e6ef32a37b09c225ef44f07a0613be3bc90847ef
SeungwooWee <slike00910@naver..com>
2021-08-14 16:15:04 +0900

    Initial commit

---
+++ codes/models/DRB_model.py
@@ -0,0 +1,173 @@
+import logging
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+from torch.nn.parallel import DataParallel, DistributedDataParallel
+import models.networks as networks
+import models.lr_scheduler as lr_scheduler
+from .base_model import BaseModel
+from models.modules.loss import CharbonnierLoss
+
+logger = logging.getLogger('base')
+
+
+class DRBModel(BaseModel):
+    def __init__(self, opt):
+        super(DRBModel, self).__init__(opt)
+
+        if opt['dist']:
+            self.rank = torch.distributed.get_rank()
+        else:
+            self.rank = -1  # non dist training
+        train_opt = opt['train']
+
+        # define network and load pretrained models
+        self.netDRB = networks.define_DRB(opt).to(self.device)
+        if opt['dist']:
+            self.netDRB = DistributedDataParallel(self.netDRB, device_ids=[torch.cuda.current_device()])
+        else:
+            self.netDRB = DataParallel(self.netDRB)
+        # print network
+        self.print_network()
+        self.load()
+
+        if self.is_train:
+            self.netDRB.train()
+
+            # loss
+            loss_type = train_opt['pixel_criterion']
+            if loss_type == 'l1':
+                self.cri_pix = nn.L1Loss().to(self.device)
+            elif loss_type == 'l2':
+                self.cri_pix = nn.MSELoss().to(self.device)
+            elif loss_type == 'cb':
+                self.cri_pix = CharbonnierLoss().to(self.device)
+            else:
+                raise NotImplementedError('Loss type [{:s}] is not recognized.'.format(loss_type))
+            self.l_pix_w = train_opt['pixel_weight']
+
+            # optimizers
+            wd_DRB = train_opt['weight_decay_DRB'] if train_opt['weight_decay_DRB'] else 0
+            optim_params = []
+            for k, v in self.netDRB.named_parameters():  # can optimize for a part of the model
+                if v.requires_grad:
+                    optim_params.append(v)
+                else:
+                    if self.rank <= 0:
+                        logger.warning('Params [{:s}] will not optimize.'.format(k))
+            self.optimizer_DRB = torch.optim.Adam(optim_params, lr=train_opt['lr_DRB'],
+                                                weight_decay=wd_DRB,
+                                                betas=(train_opt['beta1_DRB'], train_opt['beta2_DRB']))
+            self.optimizers.append(self.optimizer_DRB)
+
+            # schedulers
+            if train_opt['lr_scheme'] == 'MultiStepLR':
+                for optimizer in self.optimizers:
+                    self.schedulers.append(
+                        lr_scheduler.MultiStepLR_Restart(optimizer, train_opt['lr_steps'],
+                                                         restarts=train_opt['restarts'],
+                                                         weights=train_opt['restart_weights'],
+                                                         gamma=train_opt['lr_gamma'],
+                                                         clear_state=train_opt['clear_state']))
+            elif train_opt['lr_scheme'] == 'CosineAnnealingLR_Restart':
+                for optimizer in self.optimizers:
+                    self.schedulers.append(
+                        lr_scheduler.CosineAnnealingLR_Restart(
+                            optimizer, train_opt['T_period'], eta_min=train_opt['eta_min'],
+                            restarts=train_opt['restarts'], weights=train_opt['restart_weights']))
+            else:
+                raise NotImplementedError('MultiStepLR learning rate scheme is enough.')
+
+            self.log_dict = OrderedDict()
+
+    def feed_data(self, data, need_GT=True):
+        self.LR_data, self.GT_data = [], []
+        for idx, LR in enumerate(data['LR']):  # LR
+            self.LR_data.append([])
+            self.LR_data[idx] = LR.to(self.device)
+        if need_GT:
+            self.GT_data = data['GT'].to(self.device)  # GT
+
+    def optimize_parameters(self, step):
+        self.optimizer_DRB.zero_grad()
+        self.SR_data = self.netDRB(self.LR_data)
+        l_pix = self.l_pix_w * self.cri_pix(self.SR_data, self.GT_data)
+        l_pix.backward()
+        self.optimizer_DRB.step()
+
+        # set log
+        self.log_dict['l_pix'] = l_pix.item()
+
+    def test(self):
+        self.netDRB.eval()
+        with torch.no_grad():
+            self.SR_data = self.netDRB(self.LR_data)
+        self.netDRB.train()
+
+    def test_x8(self):
+        # from https://github.com/thstkdgus35/EDSR-PyTorch
+        self.netDRB.eval()
+
+        def _transform(v, op):
+            # if self.precision != 'single': v = v.float()
+            v2np = v.data.cpu().numpy()
+            if op == 'v':
+                tfnp = v2np[:, :, :, ::-1].copy()
+            elif op == 'h':
+                tfnp = v2np[:, :, ::-1, :].copy()
+            elif op == 't':
+                tfnp = v2np.transpose((0, 1, 3, 2)).copy()
+
+            ret = torch.Tensor(tfnp).to(self.device)
+            # if self.precision == 'half': ret = ret.half()
+
+            return ret
+
+        lr_list = [self.LR_data]
+        for tf in 'v', 'h', 't':
+            lr_list.extend([_transform(t, tf) for t in lr_list])
+        with torch.no_grad():
+            sr_list = [self.netDRB(aug) for aug in lr_list]
+        for i in range(len(sr_list)):
+            if i > 3:
+                sr_list[i] = _transform(sr_list[i], 't')
+            if i % 4 > 1:
+                sr_list[i] = _transform(sr_list[i], 'h')
+            if (i % 4) % 2 == 1:
+                sr_list[i] = _transform(sr_list[i], 'v')
+
+        output_cat = torch.cat(sr_list, dim=0)
+        self.SR_data = output_cat.mean(dim=0, keepdim=True)
+        self.netDRB.train()
+
+    def get_current_log(self):
+        return self.log_dict
+
+    def get_current_visuals(self, need_GT=True):
+        out_dict = OrderedDict()
+        out_dict['LR'] = self.LR_data.detach()[0].float().cpu()
+        out_dict['SR'] = self.SR_data.detach()[0].float().cpu()
+        if need_GT:
+            out_dict['GT'] = self.GT_data.detach()[0].float().cpu()
+        return out_dict
+
+    def print_network(self):
+        s, n = self.get_network_description(self.netDRB)
+        if isinstance(self.netDRB, nn.DataParallel) or isinstance(self.netDRB, DistributedDataParallel):
+            net_struc_str = '{} - {}'.format(self.netDRB.__class__.__name__,
+                                             self.netDRB.module.__class__.__name__)
+        else:
+            net_struc_str = '{}'.format(self.netDRB.__class__.__name__)
+        if self.rank <= 0:
+            logger.info('Network DRB structure: {}, with parameters: {:,d}'.format(net_struc_str, n))
+            logger.info(s)
+
+    def load(self):
+        load_path_DRB = self.opt['path']['pretrain_model_DRB']
+        if load_path_DRB is not None:
+            logger.info('Loading model for DRB [{:s}] ...'.format(load_path_DRB))
+            self.load_network(load_path_DRB, self.netDRB, self.opt['path']['strict_load'])
+
+    def save(self, iter_label):
+        self.save_network(self.netDRB, 'G', iter_label)
---
+++ codes/models/modules/DRBNet_arch.py
@@ -0,0 +1,433 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResModule(nn.Module):
+    def __init__(self, inout_ch, kernel_size=3, bias=False, bn=False, act=nn.PReLU(), res_scale=1):
+        super(ResModule, self).__init__()
+        module_temp = []
+        for i in range(2):
+            module_temp.append(nn.Conv2d(in_channels=inout_ch, out_channels=inout_ch, kernel_size=kernel_size, stride=1,
+                                         padding=(kernel_size // 2), bias=bias))
+            if i == 0:
+                if bn:
+                    module_temp.append(nn.BatchNorm2d(inout_ch))
+                module_temp.append(act)
+
+        self.module = nn.Sequential(*module_temp)
+        self.res_scale = res_scale
+
+        self.conv_out = nn.Conv2d(in_channels=inout_ch, out_channels=inout_ch, kernel_size=1, stride=1,
+                                  padding=0, bias=bias)
+
+    def forward(self, x):
+        res = self.module(x).mul(self.res_scale)
+        out = self.conv_out(res + x)
+        return out
+
+
+class ResModule_down(nn.Module):
+    def __init__(self, in_ch, out_ch, kernel_size=3, bias=False, bn=False, act=nn.PReLU(), res_scale=1):
+        super(ResModule_down, self).__init__()
+        module_temp_down = [nn.Conv2d(in_channels=in_ch, out_channels=in_ch, kernel_size=kernel_size, stride=2,
+                                      padding=(kernel_size // 2), bias=bias), act]
+        self.module_down = nn.Sequential(*module_temp_down)
+
+        module_temp = []
+        for i in range(2):
+            module_temp.append(nn.Conv2d(in_channels=in_ch, out_channels=in_ch, kernel_size=kernel_size, stride=1,
+                                         padding=(kernel_size // 2), bias=bias))
+            if i == 0:
+                if bn:
+                    module_temp.append(nn.BatchNorm2d(in_ch))
+                module_temp.append(act)
+
+        self.module = nn.Sequential(*module_temp)
+        self.res_scale = res_scale
+
+        self.conv_out = nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=1, stride=1,
+                                  padding=0, bias=bias)
+
+    def forward(self, x):
+        x = self.module_down(x)
+        res = self.module(x) * self.res_scale
+        out = self.conv_out(res + x)
+        return out
+
+
+class scale_up_x2(nn.Module):
+    def __init__(self, ch):
+        super(scale_up_x2, self).__init__()
+        self.deconv = nn.ConvTranspose2d(ch, ch, 3, stride=2, padding=1)
+
+    def forward(self, x):
+        out = self.deconv(x, output_size=[x.size(0), x.size(1), x.size(2) * 2, x.size(3) * 2])
+
+        return out
+
+
+class CALayer(nn.Module):
+    def __init__(self, inout_ch, ch_reduction):
+        super(CALayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv_du = nn.Sequential(
+            nn.Conv2d(inout_ch, inout_ch // ch_reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inout_ch // ch_reduction, inout_ch, 1, padding=0, bias=True),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+
+class U_shaped_Net_with_CA_dense(nn.Module):
+    def __init__(self, ch, bias, bn, act, res_scale, ch_reduction):
+        super(U_shaped_Net_with_CA_dense, self).__init__()
+        self.resB_down1 = ResModule_down(in_ch=ch, out_ch=4 * ch, bias=bias, bn=bn, act=act, res_scale=res_scale)
+        self.resB_down2 = ResModule_down(in_ch=4 * ch, out_ch=16 * ch, bias=bias, bn=bn, act=act, res_scale=res_scale)
+        self.deconv3 = nn.ConvTranspose2d(16 * ch, 4 * ch, 3, stride=2, padding=1)
+
+        self.resB4 = ResModule(inout_ch=8 * ch, bias=bias, bn=bn, act=act, res_scale=res_scale)
+        self.deconv5 = nn.ConvTranspose2d(8 * ch, 2 * ch, 3, stride=2, padding=1)
+        self.resB6 = ResModule(inout_ch=3 * ch, bias=bias, bn=bn, act=act, res_scale=res_scale)
+        self.CA = CALayer(inout_ch=3 * ch, ch_reduction=ch_reduction)
+        self.conv_out = nn.Conv2d(in_channels=4 * ch, out_channels=ch, kernel_size=3, stride=1, padding=1,
+                                  bias=False)
+
+    def forward(self, x):
+        cat1 = x[0]
+        x[0] = self.resB_down1(x[0])
+        cat2 = x[0]
+        x[0] = self.resB_down2(x[0])
+        x[0] = self.deconv3(x[0], output_size=[x[0].size(0), x[0].size(1), x[0].size(2) * 2, x[0].size(3) * 2])
+        x[0] = torch.cat((cat2, x[0]), 1)
+        x[0] = self.resB4(x[0])
+        x[0] = self.deconv5(x[0], output_size=[x[0].size(0), x[0].size(1), x[0].size(2) * 2, x[0].size(3) * 2])
+        x[0] = torch.cat((cat1, x[0]), 1)
+        x[0] = self.resB6(x[0])
+        x[0] = self.CA(x[0])
+        out = self.conv_out(torch.cat((x[0], x[1]), 1))
+        return [out, x[1]]
+
+
+class img_to_feat(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(img_to_feat, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, stride=1, padding=1),
+            nn.PReLU(),
+            nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, stride=1, padding=1)
+        )
+
+    def forward(self, x):
+        out = self.features(x)
+        return out
+
+
+### final model
+class DRBNet_mid(nn.Module):
+    def __init__(self):
+        super(DRBNet_mid, self).__init__()
+        use_act = nn.PReLU()
+        use_bias = False
+        use_bn = False
+
+        res_scale = 0.1
+        block_n = 2
+
+        ch = 64
+        ch_reduction = 16
+        self.in_feat1 = img_to_feat(in_ch=9, out_ch=ch)
+        self.in_feat2 = img_to_feat(in_ch=9, out_ch=ch)
+        self.in_feat3 = img_to_feat(in_ch=9, out_ch=ch)
+
+        module_U_net1 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net1 = nn.Sequential(*module_U_net1)
+
+        module_U_net2 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net2 = nn.Sequential(*module_U_net2)
+
+        module_U_net3 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net3 = nn.Sequential(*module_U_net3)
+
+        self.scale_up1 = scale_up_x2(ch)
+        self.scale_up2 = scale_up_x2(ch)
+        self.scale_up3 = scale_up_x2(ch)
+
+        self.in_feat4 = img_to_feat(in_ch=9, out_ch=ch)
+        self.conv_1d = nn.Conv2d(in_channels=4 * ch, out_channels=2 * ch, kernel_size=1, stride=1, padding=0,
+                                 bias=False)
+        ch = 2 * ch
+        ch_reduction = 32
+        module_U_net4 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net4 = nn.Sequential(*module_U_net4)
+
+        self.scale_up4 = scale_up_x2(ch)
+
+        self.conv_out = nn.Conv2d(in_channels=ch, out_channels=3, kernel_size=3, stride=1, padding=1,
+                                  bias=False)
+
+        self.initialize_weights()
+
+    def forward(self, x):
+        x1 = torch.cat((x[0], x[1], x[2]), 1)
+        x2 = torch.cat((x[1], x[2], x[3]), 1)
+        x3 = torch.cat((x[2], x[3], x[4]), 1)
+
+        x1 = self.in_feat1(x1)
+        x2 = self.in_feat2(x2)
+        x3 = self.in_feat3(x3)
+
+        U1 = self.U_net1([x1, x2])
+        U2 = self.U_net2([x2, x2])
+        U3 = self.U_net3([x3, x2])
+
+        x1 = self.scale_up1(U1[0])
+        x2 = self.scale_up2(U2[0])
+        x3 = self.scale_up3(U3[0])
+
+        scale_up_x1 = F.interpolate(x[1], scale_factor=2, mode='bilinear', align_corners=True)
+        scale_up_x2 = F.interpolate(x[2], scale_factor=2, mode='bilinear', align_corners=True)
+        scale_up_x3 = F.interpolate(x[3], scale_factor=2, mode='bilinear', align_corners=True)
+
+        scale_up_x = self.in_feat4(torch.cat((scale_up_x1, scale_up_x2, scale_up_x3), 1))
+        out = self.conv_1d(torch.cat((scale_up_x, x1, x2, x3), 1))
+        out = self.U_net4([out, out])
+        # out = self.U_net4(torch.cat((scale_up_x, x1, x2, x3), 1))
+        out = self.scale_up4(out[0])
+        out = self.conv_out(out)
+        return out
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.constant_(m.bias, 0)
+
+
+class DRBNet_side_2nd(nn.Module):
+    def __init__(self):
+        super(DRBNet_side_2nd, self).__init__()
+        use_act = nn.PReLU()
+        use_bias = False
+        use_bn = False
+
+        res_scale = 0.1
+        block_n = 2
+
+        ch = 64
+        ch_reduction = 16
+        self.in_feat1 = img_to_feat(in_ch=3, out_ch=ch)
+        self.in_feat2 = img_to_feat(in_ch=9, out_ch=ch)
+        self.in_feat3 = img_to_feat(in_ch=3, out_ch=ch)
+
+        module_U_net1 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net1 = nn.Sequential(*module_U_net1)
+
+        module_U_net2 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net2 = nn.Sequential(*module_U_net2)
+
+        module_U_net3 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net3 = nn.Sequential(*module_U_net3)
+
+        self.scale_up1 = scale_up_x2(ch)
+        self.scale_up2 = scale_up_x2(ch)
+        self.scale_up3 = scale_up_x2(ch)
+
+        self.in_feat4 = img_to_feat(in_ch=9, out_ch=ch)
+        self.conv_1d = nn.Conv2d(in_channels=4 * ch, out_channels=2 * ch, kernel_size=1, stride=1, padding=0,
+                                 bias=False)
+
+        ch = 2 * ch
+        ch_reduction = 32
+        module_U_net4 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net4 = nn.Sequential(*module_U_net4)
+
+        self.scale_up4 = scale_up_x2(ch)
+
+        self.conv_out = nn.Conv2d(in_channels=ch, out_channels=3, kernel_size=3, stride=1, padding=1,
+                                  bias=False)
+
+        self.initialize_weights()
+
+    def forward(self, x):
+        x1 = x[0]
+        x2 = torch.cat((x[0], x[1], x[2]), 1)
+        x3 = x[2]
+
+        x1 = self.in_feat1(x1)
+        x2 = self.in_feat2(x2)
+        x3 = self.in_feat3(x3)
+
+        U1 = self.U_net1([x1, x2])
+        U2 = self.U_net2([x2, x2])
+        U3 = self.U_net3([x3, x2])
+
+        x1 = self.scale_up1(U1[0])
+        x2 = self.scale_up2(U2[0])
+        x3 = self.scale_up3(U3[0])
+
+        scale_up_x1 = F.interpolate(x[0], scale_factor=2, mode='bilinear', align_corners=True)
+        scale_up_x2 = F.interpolate(x[1], scale_factor=2, mode='bilinear', align_corners=True)
+        scale_up_x3 = F.interpolate(x[2], scale_factor=2, mode='bilinear', align_corners=True)
+
+        scale_up_x = self.in_feat4(torch.cat((scale_up_x1, scale_up_x2, scale_up_x3), 1))
+        out = self.conv_1d(torch.cat((scale_up_x, x1, x2, x3), 1))
+        out = self.U_net4([out, out])
+        # out = self.U_net4(torch.cat((scale_up_x, x1, x2, x3), 1))
+        out = self.scale_up4(out[0])
+        out = self.conv_out(out)
+        return out
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.constant_(m.bias, 0)
+
+
+class DRBNet_side_1st(nn.Module):
+    def __init__(self):
+        super(DRBNet_side_1st, self).__init__()
+        use_act = nn.PReLU()
+        use_bias = False
+        use_bn = False
+
+        res_scale = 0.1
+        block_n = 2
+
+        ch = 64
+        ch_reduction = 16
+        self.in_feat1 = img_to_feat(in_ch=9, out_ch=ch)
+        self.in_feat2 = img_to_feat(in_ch=3, out_ch=ch)
+        self.in_feat3 = img_to_feat(in_ch=3, out_ch=ch)
+
+        module_U_net1 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net1 = nn.Sequential(*module_U_net1)
+
+        module_U_net2 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net2 = nn.Sequential(*module_U_net2)
+
+        module_U_net3 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net3 = nn.Sequential(*module_U_net3)
+
+        self.scale_up1 = scale_up_x2(ch)
+        self.scale_up2 = scale_up_x2(ch)
+        self.scale_up3 = scale_up_x2(ch)
+
+        self.in_feat4 = img_to_feat(in_ch=9, out_ch=ch)
+        self.conv_1d = nn.Conv2d(in_channels=4 * ch, out_channels=2 * ch, kernel_size=1, stride=1, padding=0,
+                                 bias=False)
+        ch = 2 * ch
+        ch_reduction = 32
+        module_U_net4 = [
+            U_shaped_Net_with_CA_dense(ch=ch, bias=use_bias, bn=use_bn, act=use_act, res_scale=res_scale,
+                                       ch_reduction=ch_reduction)
+            for _ in range(block_n)]
+        self.U_net4 = nn.Sequential(*module_U_net4)
+
+        self.scale_up4 = scale_up_x2(ch)
+
+        self.conv_out = nn.Conv2d(in_channels=ch, out_channels=3, kernel_size=3, stride=1, padding=1,
+                                  bias=False)
+
+        self.initialize_weights()
+
+    def forward(self, x):
+        x1 = torch.cat((x[0], x[1], x[2]), 1)
+        x2 = x[1]
+        x3 = x[2]
+
+        x1 = self.in_feat1(x1)
+        x2 = self.in_feat2(x2)
+        x3 = self.in_feat3(x3)
+
+        U1 = self.U_net1([x1, x1])
+        U2 = self.U_net2([x2, x1])
+        U3 = self.U_net3([x3, x1])
+
+        x1 = self.scale_up1(U1[0])
+        x2 = self.scale_up2(U2[0])
+        x3 = self.scale_up3(U3[0])
+
+        scale_up_x1 = F.interpolate(x[0], scale_factor=2, mode='bilinear', align_corners=True)
+        scale_up_x2 = F.interpolate(x[1], scale_factor=2, mode='bilinear', align_corners=True)
+        scale_up_x3 = F.interpolate(x[2], scale_factor=2, mode='bilinear', align_corners=True)
+
+        scale_up_x = self.in_feat4(torch.cat((scale_up_x1, scale_up_x2, scale_up_x3), 1))
+        out = self.conv_1d(torch.cat((scale_up_x, x1, x2, x3), 1))
+        out = self.U_net4([out, out])
+        # out = self.U_net4(torch.cat((scale_up_x, x1, x2, x3), 1))
+        out = self.scale_up4(out[0])
+        out = self.conv_out(out)
+        return out
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                nn.init.constant_(m.bias, 0)
