57f9507555250795b9c1cbee6c6829242289549f
SeungwooWee <slike00910@naver..com>
2021-08-17 11:01:33 +0900

    before GMA addition

--- codes/train.py
+++ codes/train.py
@@ -182,8 +182,15 @@ def main():
                 idx = 0
                 for val_data in val_loader:
                     idx += 1
-                    img_name = os.path.splitext(os.path.basename(val_data['LQ_path'][0]))[0]
-                    img_dir = os.path.join(opt['path']['val_images'], img_name)
+                    if opt['datasets']['val']['position'] == 'mid':
+                        file_idx = 2
+                    elif opt['datasets']['val']['position']== 'side_2nd':
+                        file_idx = 1
+                    elif opt['datasets']['val']['position'] == 'side_1st':
+                        file_idx = 0
+                    img_name = os.path.splitext(os.path.basename(val_data['LR_path'][file_idx][0]))[0]
+                    # img_dir = os.path.join(opt['path']['val_images'], img_name)
+                    img_dir = opt['path']['val_images']
                     util.mkdir(img_dir)
 
                     model.feed_data(val_data)
@@ -194,8 +201,10 @@ def main():
                     gt_img = util.tensor2img(visuals['GT'])  # uint8
 
                     # Save SR images for reference
-                    save_img_path = os.path.join(img_dir,
-                                                 '{:s}_{:d}.png'.format(img_name, current_step))
+                    # save_img_path = os.path.join(img_dir,
+                    #                              '{:s}_{:d}.png'.format(img_name, current_step))
+                    save_img_path = os.path.join(img_dir, '{:s}_{:s}_{:d}.png'.format(
+                        val_data['LR_path'][file_idx][0].split('\\')[-2], img_name, current_step))
                     util.save_img(sr_img, save_img_path)
 
                     # calculate PSNR
